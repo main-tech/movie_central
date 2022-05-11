ee28569e3ef3c350bd680ca6b962014d2410257a
Mandela Mitau <adrenaline@localhost.localdomain>
2022-05-11 22:40:45 +0300

    change

--- src/App.js
+++ src/App.js
@@ -13,44 +13,47 @@ const [movies,setMovies]= useState([]);
 const [searchTerm,setSearchTerm]=useState("");
 
   useEffect(() => {
-    searchMovies("multiverse");
+    searchMovies("attack on titan");
   }, []);
   
 const searchMovies=async(title)=>{
 const response= await fetch(`${API_URL}&s=${title}`);
 const data=await response.json();
+
 setMovies(data.Search);
 
     };
   
 return(
-  <div className="app">
-  <h1>MovieCentral</h1>
-  <div className="search">
-  <input
-  placeholder="search for movies and games"
-  value={searchTerm}
-  onChange={(e)=> setSearchTerm(e.target.value)}
-  />
-   <img
-   src={SearchIcon}
-   alt="search"
-   onClick={()=>searchMovies(searchTerm)}
-   />
-   </div>
-   
-   {
-   movies?.length > 0 ?(
-     <div className="container">
-{movies.map((movie)=>(<MovieCard movie ={movie}/>))}
-   </div>
-   ): (<div className ="empty"><h2>No movies found</h2>
-    </div>
-   )}
- 
-</div>
+    <div className="app">
+      <h1>MovieLand</h1>
 
-);
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
